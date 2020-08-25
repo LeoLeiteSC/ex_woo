f560049bb65297f2fc5baf10e364fa7b9f74ff38
Leonardo Leite <leonardo.leitesc@gmail.com>
2020-08-25 01:57:03 -0300

    Create ExWoo with basic functions

--- lib/ex_woo.ex
+++ lib/ex_woo.ex
@@ -1,18 +1,28 @@
 defmodule ExWoo do
   @moduledoc """
-  Documentation for `ExWoo`.
+  Module to make requests to WooCommerce API
   """
 
-  @doc """
-  Hello world.
+  alias ExWoo.{
+    Client,
+    Request,
+    Response,
+  }
 
-  ## Examples
+  def create(url, params, client \\ Client.build_client()), do: request(:post, url, params, client)
 
-      iex> ExWoo.hello()
-      :world
+  def get(url, params, client \\ Client.build_client()), do: request(:get, url, params, client)
 
-  """
-  def hello do
-    :world
+  defp request(method, url, params, client) do
+    client
+    |> Client.request(%Request{method: method, url: url, body: params})
+    |> handle_response()
   end
+
+  defp handle_response({:ok, %Response{status: 200, body: body}}), do: {:ok, body}
+  defp handle_response({:ok, %Response{status: 201, body: body}}), do: {:ok, body}
+  defp handle_response({:ok, %Response{status: status, body: body}}), do: {:error, %{status: status, message: body}}
+  defp handle_response({:error, _} = error), do: error
+
+  def config(key), do: Application.get_env(:ex_woo, key)
 end
